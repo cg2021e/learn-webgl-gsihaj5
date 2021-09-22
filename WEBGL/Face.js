cac654d0ab509651e2299c6f875c57b2382e6c7f
Gerry Sihaj <gsihaj5@gmail.com>
2021-09-22 16:48:52 +0700

    add: Face constructor documentation

--- WEBGL/Face.js
+++ WEBGL/Face.js
@@ -1,4 +1,10 @@
 export default class Face {
+    /*
+        @constructor
+        @param number vertice1 - index of first vertice
+        @param number vertice2 - index of second vertice
+        @param number vertice3 - index of third vertice
+     */
     constructor(vertice1, vertice2, vertice3) {
         this.vertice1 = vertice1;
         this.vertice2 = vertice2;
